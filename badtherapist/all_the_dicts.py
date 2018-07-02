3f7e96aaa9cb7b911201d2983ea13d951a246cfa
soniacreates <soniacreates@live.com>
2018-07-01 17:34:09 -0700

    stuff
    random projects

---
+++ badtherapist/all_the_dicts.py
@@ -0,0 +1,21 @@
+# ALL the dicts
+
+shawn_dict = {"sad": "happy good vibes",
+			  "happy": "so happy, so very happy for you",
+			  "angry": "girl, you need to chill out",
+			  "nervous": "breathe, baby, breathe"}
+
+pikachu_dict = {"sad": "happy good vibes",
+			  "happy": "so happy, so very happy for you",
+			  "angry": "girl, you need to chill out",
+			  "nervous": "breathe, baby, breathe"}
+
+unikitty_dict = {"sad": "happy good vibes",
+			  "happy": "Everything is AWESOME!",
+			  "angry": "girl, you need to chill out",
+			  "nervous": "breathe, baby, breathe"}
+
+brighticorn_dict = {"sad": "happy good vibes",
+			  "happy": "so happy, so very happy for you",
+			  "angry": "girl, you need to chill out",
+			  "nervous": "breathe, baby, breathe"}
